6e63328dcea795912b3d6e191901c1f65bf3932b
gonzalez2 <cgonzalez_912@yahoo.com>
2015-01-26 18:23:16 -0800

    update

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,27 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 public class Driver {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner keyboard = new Scanner(System.in);
 	
 		System.out.println("Enter the name of the file");
-		String machine = scanner.nextLine();
+		String machine = keyboard.nextLine();
+		//open file
 		File file = new File(machine);
 		if(!file.exists()){
 			System.out.println("The file does not exist");
 			System.exit(0);
 		}
+	    Scanner input = new Scanner(file);
+	    //read file
+		while(input.hasNext()){
+			int number = input.nextInt();
+			System.out.println(number);
+		}//end of openFile
+		
+		input.close();
 	}
-
+	
 }
