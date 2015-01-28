0d7be8e977a6619bd4e440ca6d387eed141558bd
Cesar Gonzalez <gonzalezrivas912@gmail.com>
2015-01-27 17:20:35 -0800

    tranlated algorythm

--- src/ufsa.java
+++ src/ufsa.java
@@ -1,5 +1,37 @@
 
 public class ufsa {
-	
+	/*int state = initial_state;
+	boolean exit = false;
+	while(!exit)
+	{
+		int symbol = getNextSymbol();
+		if(symbol ??)
+		{
+			state = getNextState(state,symbol);
+
+			if(state is_dead)
+			{
+				exit = true;
+				system.out.print("reject");
+			}
+		}
+		else
+		{
+			exit = true;
+			if(!symbol == endmarker)
+			{
+				System,out.println("reject");
+			
+				else if(state == final)
+				{
+					System.out.println("accept");
+				}
+				else
+				{
+					System.out.println(reject);
+				}
+			}
+		}	
+	}*/
 
 }
